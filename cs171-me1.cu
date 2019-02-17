52a5d4d6f69af2f22a504f60df91a6b50dd90abe
Francis dela Cruz <franciszac.dlc@gmail.com>
2019-02-17 18:16:49 +0800

    Add benchmarking

--- cs171-me1.cu
+++ cs171-me1.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
+#include <time.h>
 
 void printDevProp(cudaDeviceProp devProp) {
     printf("%s\n", devProp.name);
@@ -28,6 +29,15 @@ void printMatrix(float *A, int dim) {
         printf("]\n");
     }
     printf("]\n");
+}
+
+void populateMatrix(float *A, int dim) {
+    // Generate the values
+    for (int i=0; i<dim; i++) {
+        for (int j=0; j<dim; j++) {
+            A[i*dim + j] = (float) rand() / (float) (RAND_MAX / 100);
+        }
+    }
 }
 
 __global__
@@ -62,7 +72,11 @@ void kernel_1t1c(float *d_A, float *d_B, float *d_C, int rows) {
     }
 }
 
-void hostFunction(float *A, float *B, float *C, int rows) {
+double hostFunction(float *A, float *B, float *C, int rows, int blockSize, int kernel_choice) {
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
     // Allocate device memory
     float *d_A, *d_B, *d_C;
 
@@ -76,13 +90,24 @@ void hostFunction(float *A, float *B, float *C, int rows) {
 
     // Call kernel function
     int size = rows*rows;
-    int numBlocks = (int) (rows/1024) + 1;
-    dim3 threadsPerBlock(1024,1);
+    int numBlocks = (int) (rows/blockSize) + 1;
+    dim3 threadsPerBlock(blockSize,1);
 
-    //kernel_1t1e<<<(int) (rows/1024) + 1, 1024>>>(d_A, d_B, d_C, size);
-    kernel_1t1r<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, rows);
-    //kernel_1t1c<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, rows);
+    cudaEventRecord(start);
+    if (kernel_choice == 0) {
+        int numBlocks = (int) (size/blockSize) + 1;
+        kernel_1t1e<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, size);
+    } else if (kernel_choice == 1) {
+        kernel_1t1r<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, rows);
+    } else if (kernel_choice == 2) {
+        kernel_1t1c<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, rows);
+    }
+    cudaEventRecord(stop);
     cudaDeviceSynchronize();
+
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
 
     // Get return value
     cudaMemcpy(A, d_A, rows*rows*sizeof(float), cudaMemcpyDeviceToHost);
@@ -91,50 +116,58 @@ void hostFunction(float *A, float *B, float *C, int rows) {
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
+
+    return milliseconds;
 }
 
 int main() {
     // Device Query first
     int deviceCount;
+    int blockSize = 1024;
     cudaGetDeviceCount(&deviceCount);
     int device;
     for (device = 0; device < deviceCount; ++device) {
         cudaDeviceProp deviceProp;
         cudaGetDeviceProperties(&deviceProp, device);
         printDevProp(deviceProp);
+        blockSize = deviceProp.maxThreadsPerBlock;
     }
 
     // In my (Francis) local machine there is only one CUDA machine, so I'll hardcode that one here
     // Allocate memory
-    const int rows = 16;
+    const int rows = 64;
     const int cols = rows;
     float *A, *B, *C;
     A = (float*) malloc(sizeof(float) * rows * cols);
     B = (float*) malloc(sizeof(float) * rows * cols);
     C = (float*) malloc(sizeof(float) * rows * cols);
 
-    // Generate the values
-    for (int i=0; i<rows; i++) {
-        for (int j=0; j<cols; j++) {
-            B[i*rows + j] = (float) rand() / (float) (RAND_MAX / 100);
-            C[i*rows + j] = (float) rand() / (float) (RAND_MAX / 100);
-        }
-    }
-
     // Call the host function
-    hostFunction(A, B, C, rows);
+    // Benchmarking
+    int kernel = 0;
+    int runs = 10;
+    double time_spent = 0.0;
+    double ave_time = 0.0;
+    printf("\n");
 
-    printf("A:\n");
-    printMatrix(A, rows);
-    printf("B:\n");
-    printMatrix(B, rows);
-    printf("C:\n");
-    printMatrix(C, rows);
+    while (kernel < 3) {
+        printf("#%d:\t", kernel);
+        for (int run=0; run<runs; run++) {
+            populateMatrix(B, rows);
+            populateMatrix(C, rows);
+            time_spent = hostFunction(A, B, C, rows, blockSize, kernel);
+            ave_time += time_spent;
+            printf("%.4f\t", time_spent);
+        }
+        ave_time /= runs;
+        printf("Ave: %.4f\n", ave_time);
+        kernel++;
+    }
 
     // Free memory
     free(A);
     free(B);
     free(C);
 
-    printf("Done!\n");
+    printf("\nDone!\n");
 }
